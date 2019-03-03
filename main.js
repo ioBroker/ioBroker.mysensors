fcb427d6bc8db9b713c4cbeeee57ffb21eb72a11
Ingo Fischer <github@fischer-ka.de>
2019-03-03 22:44:29 +0100

    add adapter-core

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 'use strict';
 
 // you have to require the utils module and call adapter function
-var utils      = require(__dirname + '/lib/utils'); // Get common adapter utils
+var utils = require('@iobroker/adapter-core'); // Get common adapter utils
 var serialport;
 var Parses     = require('sensors');
 var MySensors  = require(__dirname + '/lib/mysensors');
